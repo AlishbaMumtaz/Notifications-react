2caa6cc06e02e94c0deea8bca12e41323149bb89
Alishba Mumtaz <113450434+AlishbaMumtaz@users.noreply.github.com>
2024-01-15 19:39:54 +0500

    Add files via upload

---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+// src/components/MainComponent.js
+import React from 'react';
+import NotificationComponent from './NotificationComponent';
+
+const MainComponent = () => {
+  return (
+    <div>
+      <h1>Billsavvy notifications alternative yayyyyyyyyyyy</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quibusdam sunt quisquam, ratione in autem, exercitationem quia similique, unde quas explicabo cum magni incidunt voluptatem voluptate ipsa saepe consectetur eos inventore laudantium? Delectus temporibus amet recusandae sit numquam, nulla dolorum quidem voluptatum? Quidem magni consequuntur natus illum dolore tempora sunt eos eius. Provident quam quo voluptatem iste molestiae magnam totam asperiores atque ducimus distinctio aliquid at quibusdam nihil unde vero similique, ab harum dolorem, incidunt dolor nobis facilis? Fugiat, doloribus non. Explicabo minus eveniet voluptatum eius reprehenderit. Deleniti esse ex vero repudiandae iusto, architecto sapiente, dolorem aliquam, quia excepturi consequatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam qui inventore labore blanditiis aut eaque consectetur dignissimos quae dolor? Nesciunt, doloribus quibusdam ut incidunt et alias fuga explicabo libero doloremque voluptatem atque consectetur eius earum sint molestias quia? Nihil molestias natus tempore tempora veniam cumque, ipsum fugiat provident deserunt laudantium non laboriosam asperiores corporis. Doloremque libero ab aliquid nemo excepturi. Nemo necessitatibus dicta deleniti dolorum quasi velit cupiditate corrupti iste perferendis eaque</p>
+      <NotificationComponent />
+    </div>
+  );
+};
+
+export default MainComponent;
